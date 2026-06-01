5ccc75140850f75dd84a89b42442a1406e7f1b62
AnuragMunda <anurag.munda005@gmail.com>
2026-06-01 17:26:50 +0530

    update test

--- tests/manual/test-pdf.ts
+++ tests/manual/test-pdf.ts
@@ -2,8 +2,13 @@ import { PdfParser } from "../../src/ingestion/parsers/pdf.parser.js";
 
 const main = async () => {
   const parser = new PdfParser();
-  const result = await parser.parse("./tests/fixtures/pdfs/sample.pdf");
-  console.log(result);
+  const result = await parser.parse(
+    "./tests/fixtures/pdfs/sample-multi-page.pdf",
+  );
+
+  setTimeout(() => {
+    console.log("\n\nParsed PDF: ", result);
+  }, 1000);
 };
 
 main();
