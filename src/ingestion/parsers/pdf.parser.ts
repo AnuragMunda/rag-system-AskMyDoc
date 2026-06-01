f53e0a6407669fcccc487983ad0228443a18cadd
AnuragMunda <anurag.munda005@gmail.com>
2026-06-01 17:26:14 +0530

    extract content from pdf

--- src/ingestion/parsers/pdf.parser.ts
+++ src/ingestion/parsers/pdf.parser.ts
@@ -1,11 +1,15 @@
 import { DocumentParser } from "./parser.interface.js";
-import { ParsedDocument, SourceType } from "../types.js";
+import { ParsedDocument, ParsedSection, SourceType } from "../types.js";
 import { v4 as uuidv4 } from "uuid";
 import path from "node:path";
 import fs from "node:fs/promises";
+import { getDocument } from "pdfjs-dist/legacy/build/pdf.mjs";
+import { logger } from "@/shared/logger/logger.js";
 
 export class PdfParser implements DocumentParser {
   async parse(filePath: string): Promise<ParsedDocument> {
+    logger.info("Starting PDF ingestion\n");
+
     try {
       // Step 1: validate the file
       await this.validateFile(filePath);
@@ -13,8 +17,19 @@ export class PdfParser implements DocumentParser {
       // Step 2: read binary
       const pdfBuffer = await this.readPdf(filePath);
 
-      console.log("PDF loaded successfully");
-      console.log(`Size: ${pdfBuffer.length} bytes`);
+      logger.info(
+        { filePath, size: { bytes: pdfBuffer.length } },
+        "PDF loaded successfully",
+      );
+
+      // Step 3: Get PDF document
+      const pdf = await getDocument({
+        data: new Uint8Array(pdfBuffer),
+      }).promise;
+
+      // Step 4: Extract content from PDF, page-by-page (Preserve page boundaries)
+      const sections = await this.extractContentFromPdfPages(pdf);
+      logger.info({ totalPages: pdf.numPages }, `Content extraction completed`);
 
       // Return unified format
       return {
@@ -22,7 +37,7 @@ export class PdfParser implements DocumentParser {
         title: path.basename(filePath),
         sourceType: "pdf" as SourceType,
         source: filePath,
-        sections: [],
+        sections,
         metadata: {},
       };
     } catch (error) {
@@ -47,5 +62,29 @@ export class PdfParser implements DocumentParser {
 
   private async readPdf(filePath: string): Promise<Buffer> {
     return fs.readFile(filePath);
+  }
+
+  private async extractContentFromPdfPages(pdf: any): Promise<ParsedSection[]> {
+    const page: ParsedSection[] = [];
+
+    for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+      logger.info(`Processing page ${pageNum} of ${pdf.numPages}`);
+      
+      const pageData = await pdf.getPage(pageNum);
+      const textContent = await pageData.getTextContent();
+      const pageText = textContent.items.map((item: any) => item.str).join(" ");
+
+      if (pageText.trim().length === 0) {
+        logger.warn(`Page ${pageNum} is empty or contains non-text content`);
+      }
+
+      page.push({
+        content: pageText,
+        pageNumber: pageNum,
+      });
+
+      logger.info({ characterCount: pageText.length });
+    }
+    return page;
   }
 }
