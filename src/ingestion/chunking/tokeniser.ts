f1d5796d97aaf1d35429fc382e6f46de81af4f2b
AnuragMunda <anurag.munda005@gmail.com>
2026-06-12 19:32:43 +0530

    refactor

--- src/ingestion/chunking/tokeniser.ts
+++ src/ingestion/chunking/tokeniser.ts
@@ -1,11 +1,8 @@
-import { env } from "@/config/env.js";
-import { GoogleGenAI } from "@google/genai";
-
-const ai = new GoogleGenAI({apiKey: env.GEMINI_API_KEY});
+import { gemini } from "@/config/constants.js";
 
 // Calculate the token count for a given content
 export const countTokens = async (text: string) => {
-  const { totalTokens } = await ai.models.countTokens({
+  const { totalTokens } = await gemini.models.countTokens({
     model: "gemini-3.5-flash",
     contents: text,
   });
